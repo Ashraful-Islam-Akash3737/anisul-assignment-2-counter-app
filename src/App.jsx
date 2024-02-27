ce950b2cfa870f2b55a61b7000b660967b23d685
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-27 12:56:55 +0600

    first commit and setup the project.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import './App.css'
+import Counter from './Components/Counter'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+
+      <Counter></Counter>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div> */}
+    </>
+  )
+}
+
+export default App
