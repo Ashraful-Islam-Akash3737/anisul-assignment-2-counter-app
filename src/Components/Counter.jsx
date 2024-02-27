36507c82f5064920efee0619d31b55fbe9de56ef
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-27 13:07:00 +0600

    create the html structure for this app and complete the design.

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,9 +1,17 @@
 // import React from 'react'
-
+import './Counter.css'
 export default function Counter() {
-  return (
-    <div>
-      <h3>Count is:</h3>
-    </div>
-  )
+    return (
+        <div className="counter center">
+            <h1 className="counter__title">Counter App</h1>
+            <div className="card center">
+                <h2 className="card__title">Count : 0</h2>
+                <div className="card__btns">
+                    <button className="btn card__btn">Increment <br />+</button>
+                    <button className="btn card__btn">Decrement <br />-</button>
+                    <button className="btn card__btn">Reset <br />0</button>
+                </div>
+            </div>
+        </div>
+    );
 }
